ba2f63b41c602e04dcf8468efd1fee0ca3a975a1
chan <ubuntu@ip-172-31-46-215.ap-northeast-2.compute.internal>
2022-09-05 12:31:59 +0000

    ssu_login finish

--- xv6-public/ssu_login.c
+++ xv6-public/ssu_login.c
@@ -6,36 +6,59 @@
 char userID[16][32];
 char pwdID[16][32];
 
+//"list.txt" 파일 정보를 받아와 userID, pwdID에 저장
 void get_user_list(){
 	int fd;
 	int n;
 	char buf[1024];
+	const char *t=buf;
 
 	fd=open("list.txt",O_RDONLY);
 	n=read(fd,buf,sizeof(buf));
 
-	printf(2,"%d  %d  %s\n",fd,n,buf);
 	for(int i=0;i<16;i++){
-		
+		char *s=userID[i];
+		if(n<=0) break;
+		while(n--){
+			//nul
+			if(*t=='\0'){
+				*s=0;
+				break;
+			}
+			//SpaceBar
+			if(*t==' ') {
+				*s=0;
+				t++;
+				s=pwdID[i];
+			}
+			if(*t=='\n'){
+				*s=0;
+				t++;
+				break;
+			}
+			*s++=*t++;
+		}
 	}
 }
 
+//ID, Password 확인
 int check_idpw(char *user, char *pwd){
 	for(int i=0;i<16;i++)
 		if(!strcmp(userID[i],user)&&!strcmp(pwdID[i],pwd)) return 1;
-//	return 0;
-	return 1;
+	return 0;
 }
 
 int main(int argc,char *argv[]){
-	char user[512];
-	char pwd[512];
+	char user[512]; //Username 입력
+	char pwd[512]; //Password 입력
 	get_user_list();
 	while(1){
 		printf(2,"Username: ");
 		gets(user,sizeof(user));
+		user[strlen(user)-1]=0;
 		printf(2,"Password: ");	
 		gets(pwd,sizeof(pwd));
+		pwd[strlen(pwd)-1]=0;
 		if(check_idpw(user,pwd)) break;
 		printf(2,"login failed. try again\n");
 	}
